f3884e9a3a8b13a75a8abe2c458e3dd72db8202a
thecodeminerX <aky5271@gmail.com>
2023-02-26 17:16:28 +0530

    rendering Apis

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,5 @@ const homeController = require('../controllers/homeController');
 router.get('/', homeController.home);
 router.use('/user', require('./user'));
 router.use('/posts', require('./post'));
-
+router.use('/api', require('./api/index'));
 module.exports  = router;
