e1c9319081c6675dac49911154db171dbece42f8
Ashish Kumar <57012258+CL4P7R4P-97@users.noreply.github.com>
2023-03-04 23:24:50 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -12,8 +12,8 @@ let transporter  = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: 'ashish9780kumar',
-        pass: 'zkarosnvtyxnbvvz'
+        user: ' ',
+        pass: ' '
     }
 });
 
